fe6bc2728492f925c278c31559e0ed3ac09ad8d7
Edgarcarrog <edgc21@hotmail.com>
2023-01-31 19:25:34 -0600

    useRef example

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
+import { useEffect, useRef, useState } from "react";
+
 function App() {
+  const [name, setName] = useState("");
+  const renderCount = useRef(1);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+    console.log("renderizé");
+    console.log(inputRef);
+  });
+
   return (
     <div className="App">
       <h1>CRUD-Node</h1>
       <h2>Subtitulo</h2>
+      <input ref={inputRef} />
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <div>My name is {name}</div>
+      <div>I rendered {renderCount.current} times</div>
+      <button onClick={() => inputRef.current.focus()}>Focus</button>
     </div>
   );
 }
